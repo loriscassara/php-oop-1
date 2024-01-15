b71288eb06e49590c53a4cf4b55bfb1c6799b332
MrElle <cassaralavoro@gmail.com>
2024-01-15 01:44:55 +0100

    first commit

---
+++ Models/Dati.php
@@ -0,0 +1,24 @@
+<?php
+
+    $avatar = new Movie ("Avatar", "James Cameron", "Sci-fi/Azione", "2009");
+    $deadpool = new Movie ("Deadpool", "Tim Miller", "Azione/Commedia", "2016");
+    $venom = new Movie ("Venom", "Ruben Fleischer", "Azione/Sci-fi", "2018");
+
+    class Movie {
+        public $filmName;
+        public $director;
+        public $type;
+        public $year;
+
+        public function __construct($nome, $regista, $genere, $anno){
+            $this->filmName = $nome;
+            $this->director = $regista;
+            $this->type = $genere;
+            $this->year = $anno;
+        }
+    
+        public function getDescription(){
+            return $result = "Name: " . $this->filmName . " Director: " . $this->director . " Type: " . $this->type . " Year: " . $this->year;
+        }
+    }
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    require __DIR__ . './Models/Dati.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP Welcome</title>
+</head>
+<body>
+    <?php
+        var_dump($avatar);
+        var_dump($deadpool);
+        var_dump($venom);
+
+        echo $avatar->getDescription() . "<br>";
+        echo $deadpool->getDescription() . "<br>";
+        echo $venom->getDescription();
+    ?>
+</body>
+</html>
